99039e6c71790438da7135d5beb9937d270da4de
Jemin Kachhadiya <36146094+Jkspectre@users.noreply.github.com>
2022-11-04 09:40:44 -0400

    image downloader and image name generator script creation

---
+++ Image name generator.py
@@ -0,0 +1,15 @@
+import os
+
+input_dir = input("Input Directory Path: ")
+class_type = str(input("Type of the class: "))
+
+def image_name_generator(input_dir, class_type):
+    for dir, subdirs, files in os.walk(input_dir):
+        i = 0
+        for f in files:
+            f_new = f"{class_type}_{str(i)}.png"
+            os.rename(os.path.join(dir, f), os.path.join(dir, f_new))
+            i += 1
+            # print(f, f_new)
+
+image_name_generator(input_dir, class_type)
